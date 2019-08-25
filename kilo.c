0c887c689e002b149943daefa2b63faa9bbbc0f4
John Pavley <john@pavley.com>
2019-08-25 18:20:27 -0400

    a timeout for read

--- kilo.c
+++ kilo.c
@@ -19,6 +19,9 @@ void enableRawMode() {
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  // control characters
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -26,13 +29,16 @@ void enableRawMode() {
 int main() {
   enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+  while(1) {
+	char c = '\0';
+	read(STDIN_FILENO, &c, 1); 
     if (iscntrl(c)) {
       printf("%d\r\n", c); // print ASCII code of control char
     } else {
       printf("%d ('%c')\r\n", c, c); // print both ASCII code and char
     }
+	if (c == 'q') break;
   }
  
   return 0;
