c0b7f90f3b6254b988baa40d4452a0aaf25ed34d
John Pavley <john@pavley.com>
2019-09-10 22:11:57 -0400

    window size the easy way

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -15,6 +16,10 @@
 
 // store global state
 struct editorConfig {
+	// screen size
+	int screenrows;
+	int screencols;
+	
 	// copy of the original terminal settings
 	struct termios orig_termios;
 };
@@ -73,12 +78,25 @@ char editorReadKey() {
 	return c;
 }
 
+// get the row count and col count
+int getWindowSize(int *rows, int *cols) {
+	struct winsize ws;
+
+	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		return -1; // error
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0; // no error 
+	}
+}
+
 /** output **/
 
 // draw chars on first col of each row
 void editorDrawRows() {
 	int y;
-	for (y = 0; y < 24; y++) {
+	for (y = 0; y < E.screenrows; y++) {
 		write(STDOUT_FILENO, "~\r\n", 3);
 	}
 }
@@ -105,10 +123,16 @@ void editorProcessKeypress() {
 
 /** init **/
 
+void initEditor() {
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+		die("getWindowSize");
+	}
+}
+
 // main entry point of program
 int main() {
-  // turn off cononical mode
-  enableRawMode();
+  enableRawMode(); // turn off cononical mode
+	initEditor(); // get the window info
 
   // process key press forever!
   while(1) {
