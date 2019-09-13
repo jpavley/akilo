2a4cf9ff6f0c402536b4bff3186c013b139fc0c9
John Pavley <john@pavley.com>
2019-09-12 22:20:49 -0400

    welcome message pt 1

--- kilo.c
+++ kilo.c
@@ -11,6 +11,8 @@
 
 /** defines **/
 
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f) // bitwise-AND char with mask 00011111
 
 /** data **/
@@ -140,7 +142,15 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
-		abAppend(ab, "~", 1);
+		if (y == E.screenrows / 3) {
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome),
+				"Kilo Editor -- Version %s", KILO_VERSION);
+			if (welcomelen > E.screencols) welcomelen = E.screencols;
+			abAppend(ab, welcome, welcomelen);
+		} else {
+			abAppend(ab, "~", 1);
+		}
 
 		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1) {
