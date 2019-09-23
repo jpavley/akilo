208a5ad80b63b8e5029be9e6896156d4ae8b8a9a
John Pavley <john@pavley.com>
2019-09-22 23:11:02 -0400

    step 58

--- kilo.c
+++ kilo.c
@@ -198,15 +198,27 @@ int getWindowSize(int *rows, int *cols) {
 
 /** file i/o **/
 
-void editorOpen() {
-	char *line = "Hello, World!";
-	ssize_t linelen = 13;
-	
-	E.row.size = linelen;
-	E.row.chars = malloc(linelen + 1);
-	memcpy(E.row.chars, line, linelen);
-	E.row.chars[linelen] = '\0';
-	E.numrows = 1;
+void editorOpen(char *filename) {
+	FILE *fp = fopen(filename, "r");
+	if (!fp) die("fopen");
+
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	linelen = getline(&line, &linecap, fp);
+	if (linelen != -1) {
+		while (linelen > 0 && (line[linelen - 1] == '\n' ||
+													 line[linelen - 1] == '\r')) {
+			linelen--;
+		}
+		E.row.size = linelen;
+		E.row.chars = malloc(linelen + 1);
+		memcpy(E.row.chars, line, linelen);
+		E.row.chars[linelen] = '\0';
+		E.numrows = 1;
+	}
+	free(line);
+	fclose(fp);
 }
 
 /** append buffer **/
@@ -375,10 +387,12 @@ void initEditor() {
 }
 
 // main entry point of program
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode(); // turn off cononical mode
 	initEditor(); // get the window info
-	editorOpen(); // open and read a file
+	if (argc >= 2) {
+		editorOpen(argv[1]); // open and read a file
+	}
 
   // process key press forever!
   while(1) {
